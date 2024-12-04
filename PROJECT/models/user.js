24925c0d22029272b317b6778d72175d0f8e9f2b
MaazSiddiquiCS <muhammadmaazsiddiqi@gmail.com>
2024-12-04 10:48:20 +0500

    Add files via upload

---
+++ PROJECT/models/user.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+
+
+
+// POST new user (signup)
+router.post('/signup', async (req, res) => {
+    try {
+        const newUser = await userController.signup(req.body);
+        res.status(201).json(newUser);
+    } catch (error) {
+        console.error('Error creating user:', error);
+        res.status(500).json({ error: 'Failed to create user' });
+    }
+});
+
+// POST login user
+router.post('/login', async (req, res) => {
+    try {
+        const user = await userController.login(req.body);
+        res.status(200).json(user);
+    } catch (error) {
+        console.error('Error logging in user:', error);
+        res.status(500).json({ error: 'Failed to log in user' });
+    }
+});
+
+module.exports = router;
