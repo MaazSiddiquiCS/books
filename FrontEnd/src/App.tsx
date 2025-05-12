90b6906fd42a460aab73c8efd9eecfd2d3ec0295
MaazSiddiquiCS <muhammadmaazsiddiqi@gmail.com>
2025-05-12 22:45:14 +0500

    Update App.tsx

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -11,18 +11,16 @@ import ReadLater from './pages/ReadLater';
 import Settings from './pages/Settings';
 import Help from './pages/Help';
 import LoginModal from './components/LoginModal';
-import ExploreNow from './pages/ExploreNow';
-import BookDetail from './pages/BookDetails';
-import Search from './components/Search';
-import ReaderPage from './pages/ReaderPage';
-import DownloadPage from './pages/DownloadPage';
-import Admin from './pages/Admin';
-import AdminLoginModal from './components/AdminLoginModal';
+import ExploreNow from 'pages/ExploreNow';
+import BookDetail from 'pages/BookDetails';
+import Search from 'components/Search';
+import ReaderPage from 'pages/ReaderPage';
+import DownloadPage from 'pages/DownloadPage';
+import Admin from 'pages/Admin';
 
 const App: React.FC = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
-  const [isAdminModalOpen, setIsAdminModalOpen] = useState(false);
 
   return (
     <Router>
@@ -32,7 +30,6 @@ const App: React.FC = () => {
           <Navbar 
             openSidebar={() => setIsSidebarOpen(true)} 
             openLoginModal={() => setIsLoginModalOpen(true)}
-            openAdminModal={() => setIsAdminModalOpen(true)}
           />
           <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-100">
             <div className="container mx-auto px-6 py-8">
@@ -44,24 +41,20 @@ const App: React.FC = () => {
                 <Route path="/read-later" element={<ReadLater />} />
                 <Route path="/settings" element={<Settings />} />
                 <Route path="/help" element={<Help />} />
-                <Route path="/explore-now" element={<ExploreNow />} />
-                <Route path="/book-detail/:id" element={<BookDetail />} />
+                <Route path="/ExploreNow" element={<ExploreNow />} />
+                <Route path="/BookDetail/:id" element={<BookDetail />} />
+                <Route path="/" element={<Home />} />
                 <Route path="/search" element={<Search />} />
-                <Route path="/reader/:id" element={<ReaderPage />} />
+                <Route path="/reader/:id" element={<ReaderPage />} /> 
+                <Route path="/footer" element={<Footer />} />
                 <Route path="/download/:id" element={<DownloadPage />} />
-                <Route path="/admin" element={<Admin />} />
+                <Route path="/admin/" element={<Admin />} />            
               </Routes>
             </div>
           </main>
-          <Footer />
         </div>
       </div>
       <LoginModal isOpen={isLoginModalOpen} setIsOpen={setIsLoginModalOpen} />
-      <AdminLoginModal 
-        isOpen={isAdminModalOpen} 
-        setIsOpen={setIsAdminModalOpen}
-        onAdminLogin={() => setIsAdminModalOpen(false)}
-      />
     </Router>
   );
 }
