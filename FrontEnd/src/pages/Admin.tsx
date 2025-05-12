21db9fef0a21b07d38f95f8cea515a6c34dfc5bb
MaazSiddiquiCS <muhammadmaazsiddiqi@gmail.com>
2025-05-12 23:57:04 +0500

    Update Admin.tsx

--- FrontEnd/src/pages/Admin.tsx
+++ FrontEnd/src/pages/Admin.tsx
@@ -80,7 +80,7 @@ const Admin: React.FC = () => {
           ...(isRegister && { 
             name: `Admin-${loginForm.id}`,
             role: 'admin',
-            const logged_in = new Date().toISOString().slice(0, 19).replace('T', ' ');
+            const logged_in: new Date().toISOString().slice(0, 19).replace('T', ' '),
           }),
           ...(!isRegister && { password: loginForm.password })
         }),
